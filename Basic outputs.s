59f8d944e759798d53bafd8b5d299319bd992a2a
xdeng0 <65934994+xdeng0@users.noreply.github.com>
2020-10-16 15:35:27 -0700

    Improve readability

--- Basic outputs.s
+++ Basic outputs.s
@@ -4,62 +4,60 @@
 # Day: 86400
 # Week: 604800
 
-.data
+# define system call code
+.eqv print_int 1
+.eqv sys_exit 10
+.eqv print_str 4
 
-#variable declaration
-Hour: .asciiz "Hour: "
-Day: .asciiz "Day: "
-Week: .asciiz "Week: "
-newLine: .asciiz "\n" # new line character
+        .data
 
-.text
+hour: .asciiz "Hour: "
+day: .asciiz "Day: "
+week: .asciiz "Week: "
+new_line: .asciiz "\n"
+
+        .text
+
 main:
 
-# prints "Hour: "
-li $v0, 4
-la $a0, Hour
-syscall
+        li      $v0, print_str          # print "Hour: "
+        la      $a0, hour
+        syscall
 
-li $t0, 60 # number of seconds in minute
-mul $t1, $t0, $t0 # the number of seconds in an hour = 60 * 60, stores the value in $t1
+        li      $t0, 60                 # load the number of seconds in a minute into $t0
+        mul     $t1, $t0, $t0           # an hour has 60 * 60 secs, load the value into $t1
 
-# prints the number of seconds in an hour
-li $v0, 1
-move $a0, $t1
-syscall
+        li      $v0, print_int          # print the number of seconds in an hour
+        move    $a0, $t1
+        syscall
 
-# prints a new line
-la $a0, newLine
-li $v0, 4
-syscall
+        la      $a0, new_line           # print a new line
+        li      $v0, print_str
+        syscall
 
-# prints "Day: "
-la $a0, Day
-li $v0, 4
-syscall
+        la      $a0, day                # print "Day: "
+        li      $v0, print_str
+        syscall
 
-mul $t2, $t1, 24 # the number of seconds in a day = number of seconds in an hour * 24
+        mul     $t2, $t1, 24            # number of seconds in a day = number of seconds in an hour * 24
 
-# prints the number of seconds in a day
-li $v0, 1
-move $a0, $t2
-syscall
+        li      $v0, print_int          # print the number of seconds in a day
+        move    $a0, $t2
+        syscall
 
-# prints a new line
-la $a0, newLine
-li $v0, 4
-syscall
+        la      $a0, new_line           # print a new line
+        li      $v0, print_str
+        syscall
 
-# prints "Week: "
-la $a0, Week
-li $v0, 4
-syscall
+        la      $a0, week               # print "Week: "
+        li      $v0, print_str
+        syscall
 
-mul $t3, $t2, 7 # the number of seconds in a week = number of seconds in a day * 7
+        mul     $t3, $t2, 7             # the number of seconds in a week = number of seconds in a day * 7
 
-# prints the number of seconds in a week
-move $a0, $t3
-li $v0, 1
-syscall
+        move    $a0, $t3                # print the number of seconds in a week
+        li      $v0, print_int
+        syscall
 
-# End of program
+        li      $v0, sys_exit
+        syscall                         # program exists
